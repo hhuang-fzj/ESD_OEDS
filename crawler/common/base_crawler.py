d6ff9fd48b64fb0e82edff501f97d459b7f10d22
Florian Maurer <maurer@fh-aachen.de>
2025-08-19 21:17:34 +0200

    clean up repository - add REUSE compliance

--- crawler/common/base_crawler.py
+++ crawler/common/base_crawler.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Florian Maurer
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 import logging
 from datetime import date, datetime, timedelta
 from pathlib import Path
--- crawler/ninja.py
+++ crawler/ninja.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: Christian Rieke, Florian Maurer
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
 import logging
 import zipfile
 from io import BytesIO
